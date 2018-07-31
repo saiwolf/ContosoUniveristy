baed481833167c033461b93ec6d86c3daa75e06a
Robert Cato <rcato@ci.thibodaux.la.us>
2018-07-31 10:38:14 -0500

    Enhanced Student Model with DataAnnotations

--- ContosoUniveristy/Models/Student.cs
+++ ContosoUniveristy/Models/Student.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniveristy.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters.")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
