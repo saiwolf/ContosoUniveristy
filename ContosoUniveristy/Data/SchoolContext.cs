abeec16785eb6a0383e283cadecdd6331e679484
Robert Cato <rcato@ci.thibodaux.la.us>
2018-07-31 08:35:31 -0500

    Scaffolded Student Model

---
+++ ContosoUniveristy/Data/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniveristy.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniveristy.Models.Student> Student { get; set; }
+    }
+}
